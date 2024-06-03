70599ee475eb32a1af2409740af0693c84485bd4
j-ackyao <wfy1001@gmail.com>
2024-06-02 18:33:05 -0700

    fixed routing from frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,20 @@ import HomePage from './pages/HomePage';
 import QuizPage from './pages/QuizPage';
 import TranslationPage from './pages/TranslationPage';
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <div className="App">
         <Routes>
           <Route path="/" exact Component={HomePage}></Route>
           <Route path="/translate" Component={TranslationPage}></Route>
           <Route path="/quiz" Component={QuizPage}></Route>
-        </Routes >
-      </div >
-    </Router>
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
